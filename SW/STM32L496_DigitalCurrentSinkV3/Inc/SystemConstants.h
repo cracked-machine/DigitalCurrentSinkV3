dccfc6caab68d4def406bfd220d2b2f6009af23d
cracked-machine <chrisjsutton@gmail.com>
2019-12-02 11:58:02 +0000

    updated

--- SW/STM32L496_DigitalCurrentSinkV3/Inc/SystemConstants.h
+++ SW/STM32L496_DigitalCurrentSinkV3/Inc/SystemConstants.h
@@ -28,7 +28,7 @@
 #define MAXAMPSTEP 			1
 
 // adjust to match power resistor value, allows for variation according to tolerances
-#define CH0_PWR_RESISTOR	0.15 	// positive N-channel
+#define CH0_PWR_RESISTOR	0.12 	// positive N-channel
 #define CH0_RESISTOR_ERR	0.0	// tolerance compensation
 #define CH1_PWR_RESISTOR	0.1 	// negative P-channel
 #define CH1_RESISTOR_ERR	0.00	// tolerance compensation
@@ -36,7 +36,7 @@
 /* instrumentation amp gain compensation.
  * This can be done in HW using gain resistor or "fixed" here in SW
  * Absolute minimum value is 1 */
-#define CH0_INAMP_GAIN_COMP		6.75
+#define CH0_INAMP_GAIN_COMP		8.8
 #define CH1_INAMP_GAIN_COMP		6.8
 
 /* prescaler for RAND and AUTO modes. RAND PSC should be much larger than AUTO PSC */
